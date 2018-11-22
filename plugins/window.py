988c31751f0061b3208257bc524e070694b01cd4
rjmcf <rjmcf@live.co.uk>
2018-11-22 13:04:48 +0000

    Refactor window to be neater

--- plugins/window.py
+++ plugins/window.py
@@ -18,22 +18,22 @@ class TopLevelWindow():
 # Windows are hierarchical, and so have a list of child_windows that they manage.
 #TODO Investigate: Validate on whether stuff is outside the parent?
 class Window():
-	def __init__(self, x_prop, y_prop, width_prop, height_prop, parent_x = -1, parent_y = -1, parent_width = -1, parent_height = -1):
+	def __init__(self, x_prop, y_prop, width_prop, height_prop, parent_x = None, parent_y = None, parent_width = None, parent_height = None):
 		self.x_prop = x_prop
 		self.y_prop = y_prop
 		self.width_prop = width_prop
 		self.height_prop = height_prop
 		
-		if parent_x != -1:
-			self.x = parent_x
-		if parent_y != -1:
-			self.y = parent_y
-		if parent_width != -1:
-			self.width = parent_width
-		if parent_height != -1:
-			self.height = parent_height
+		if None not in [parent_x, parent_y, parent_width, parent_height]:
+			self.calculate_dimensions(parent_x, parent_y, parent_width, parent_height)
 		
 		self.child_windows = []
+		
+	def calculate_dimensions(self, parent_x, parent_y, parent_width, parent_height):
+		self.x = parent_x + parent_width * self.x_prop
+		self.y = parent_y + parent_height * self.y_prop
+		self.width = parent_width * self.width_prop
+		self.height = parent_height * self.height_prop
 		
 	# Called every frame to update the contents	
 	def update(self):
@@ -41,10 +41,7 @@ class Window():
 			window.update()
 		
 	def draw(self, parent_x, parent_y, parent_width, parent_height):
-		self.x = parent_x + parent_width * self.x_prop
-		self.y = parent_y + parent_height * self.y_prop
-		self.width = parent_width * self.width_prop
-		self.height = parent_height * self.height_prop
+		self.calculate_dimensions(parent_x, parent_y, parent_width, parent_height)
 		self.draw_before_children()
 		for child in self.child_windows:
 			child.draw(self.x, self.y, self.width, self.height)
